93bbd0b81f6e135705a0dc99301e2bfb519f6a92
Talib Zaz <musist222@gmail.com>
2019-08-10 20:43:08 +0530

    generate invoice + add items to sidebar

--- sales/models.py
+++ sales/models.py
@@ -2,25 +2,29 @@ from django.db import models
 
 
 class Customer(models.Model):
+    cat_choices = [('G', 'General'), ('A', 'Category A'), ('B', 'Category B'), ('C', 'Category C')]
+
     name = models.CharField(max_length=255, null=False)
     firm_name = models.CharField(max_length=255, null=False)
     address = models.TextField()
     gstin = models.CharField(max_length=15, null=True)
     primary_num = models.IntegerField(null=False)
     secondary_num = models.IntegerField(null=True)
-    category = models.CharField(max_length=20, choices=[('A', 'Category A'), ('B', 'Category B'), ('C', 'Category C')])
+    category = models.CharField(max_length=20, choices=cat_choices)
 
     def __str__(self):
         return self.name
 
 
 class Sales(models.Model):
+    order_id = models.CharField(max_length=20)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
-    sub_total = models.IntegerField(null=False)
-    shipping_charges = models.IntegerField(null=True)
-    date = models.DateTimeField()
-    amount_paid = models.IntegerField()
-    remaining_bal = models.IntegerField()
+    sub_total = models.FloatField(null=False)
+    p_and_f = models.FloatField(null=True)
+    round_off = models.FloatField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    amount_paid = models.FloatField()
+    remaining_bal = models.FloatField()
 
     def __str__(self):
-        return self.customer_id, self.amount_paid
+        return self.order_id
--- sales/static/js/invoice.js
+++ sales/static/js/invoice.js
@@ -1,50 +1,42 @@
-function createItemsTable() {
-            //$('#tbody').empty();
-            let rowCount = parseInt($('#tbody tr').length);
-            let items_count = $('#total_items').val();
-            if(rowCount === 0){
-                for(let i=1; i<=items_count; i++){
-                    $('#tbody').append("<tr id='tr_item_"+i+"'>"+
-                        "<td>"+i+"</td>"+
-                        "<td><input class='form-control' type='text' id='item_name_"+i+"'></td>"+
-                        "<td><input class='form-control' type='number' value=1 id='item_quantity_"+i+"' value onchange='calculate_total(this.id)'></td>"+
-                        "<td><input class='form-control' type='number' id='item_mrp_"+i+"' value onchange='calculate_total(this.id)'></td>"+
-                        "<td><input class='form-control' type='number' id='item_discount_"+i+"' value onchange='calculate_total(this.id)'></td>"+
-                        "<td class='text-center' style='padding-top:15px'><b><span id='item_total_"+i+"'></span></b></td>"+
-                        "<td class='text-center'>" +
-                            "<a class='btn' id="+i+" onclick='clearRowData(this.id)'><i class='fa fa-clear'></i>x</a>" +
-                        "</td>"+
-                        "</tr>"
-                    );
-                }
-            } else {
-                for(let i=rowCount+1; i <= rowCount+parseInt(items_count); i++){
-                    $('#tbody').append("<tr id='tr_item_"+i+"'>"+
-                        "<td>"+i+"</td>"+
-                        "<td><input class='form-control' type='text' id='item_name_"+i+"'></td>"+
-                        "<td><input class='form-control' type='number' value=1 id='item_quantity_"+i+"' value onchange='calculate_total(this.id)'></td>"+
-                        "<td><input class='form-control' type='number' id='item_mrp_"+i+"' value onchange='calculate_total(this.id)'></td>"+
-                        "<td><input class='form-control' type='number' id='item_discount_"+i+"' value onchange='calculate_total(this.id)'></td>"+
-                        "<td class='text-center' style='padding-top:15px'><b><span id='item_total_"+i+"'></span></b></td>"+
-                        "<td class='text-center'>" +
-                            "<a class='btn' id="+i+" onclick='clearRowData(this.id)'><i class='fa fa-clear'></i>x</a>" +
-                        "</td>"+
-                        "</tr>"
-                    );
-                }
-            }
+function generateTable(i){
+    $('#tbody').append("<tr id='tr_item_"+i+"'>"+
+        "<td class='text-center' style='padding-top:15px;'>"+i+"</td>"+
+        "<td><input class='form-control' type='text' id='item_name_"+i+"'></td>"+
+        "<td><input class='form-control' type='number' value=1 id='item_quantity_"+i+"' value onchange='calculate_row_total(this.id)'></td>"+
+        "<td><input class='form-control' type='number' id='item_mrp_"+i+"' value onchange='calculate_row_total(this.id)'></td>"+
+        "<td><input class='form-control' type='number' id='item_discount_"+i+"' value onchange='calculate_row_total(this.id)'></td>"+
+        "<td class='text-center' style='padding-top:15px'><b><span id='item_total_"+i+"'></span></b></td>"+
+        "<td class='text-center'>" +
+            "<a class='btn' id="+i+" onclick='clearRowData(this.id)'><i class='fa fa-clear'></i>x</a>" +
+        "</td>"+
+        "</tr>"
+    );
+}
 
+function addMultipleRows() {
+    let rowCount = parseInt($('#tbody tr').length);
+    let items_count = parseInt($('#total_items').val());
+    if(rowCount === 0){
+        for(let i=1; i<=items_count; i++){
+            generateTable(i)
         }
+    } else {
+        for(let i=rowCount+1; i <= rowCount+items_count; i++){
+            generateTable(i)
+        }
+    }
+}
 
-function calculate_total(e){
-    let res = e.split('_');
+function addOneRow() {
+    let rowCount = parseInt($('#tbody tr').length);
+    if (rowCount === 0){
+        generateTable(1)
+    } else {
+        generateTable(rowCount+1)
+    }
+}
 
-    let quantity = $('#item_quantity_'+res[2]).val();
-    let mrp = $('#item_mrp_'+res[2]).val();
-    let discount = $('#item_discount_'+res[2]).val();
-    let total = (quantity * mrp) - ((quantity * mrp * discount) / 100);
-    $('#item_total_'+res[2]).empty().append(total);
-
+function calculateSubTotal() {
     let sub_total = 0;
     let items_count = parseInt($('#tbody tr').length);
     for(let i=1; i <= items_count; i++){
@@ -54,15 +46,24 @@ function calculate_total(e){
         }
     }
     $('#sub_total').empty().append(sub_total);
-    let total_amount = sub_total + parseFloat($('#last_balance').val()) + parseFloat($('#shipping_cost').val());
-    $('#total_amount').empty().append(total_amount);
-    let remaining_balance = total_amount - parseFloat($('#amount_paid').val());
-    $('#remaining_balance').empty().append(remaining_balance)
+    calculateGrandTotal();
+}
+
+function calculate_row_total(e){
+    let res = e.split('_');
+
+    let quantity = $('#item_quantity_'+res[2]).val();
+    let mrp = $('#item_mrp_'+res[2]).val();
+    let discount = $('#item_discount_'+res[2]).val();
+    let total = (quantity * mrp) - ((quantity * mrp * discount) / 100);
+    $('#item_total_'+res[2]).empty().append(total);
+
+    calculateSubTotal();
 }
 
 function calculateGrandTotal() {
-    let sub_total = parseFloat($('#sub_total').text());
-    let total_amount = sub_total + parseFloat($('#last_balance').val()) + parseFloat($('#shipping_cost').val());
+    let total_amount = parseFloat($('#sub_total').text()) + parseFloat($('#last_balance').val()) +
+        parseFloat($('#p_and_f').val()) + parseFloat($('#round_off').val()) ;
     $('#total_amount').empty().append(total_amount);
     let remaining_balance = total_amount - parseFloat($('#amount_paid').val());
     $('#remaining_balance').empty().append(remaining_balance)
@@ -74,12 +75,14 @@ function clearRowData(id) {
     $('#item_mrp_'+id).val('');
     $('#item_discount_'+id).val('');
     $('#item_total_'+id).text('');
+    $('#round_off').val(0);
+    calculateSubTotal();
 }
 
 function customerChange() {
     let customer_type = $('#customer_type').val();
     if(customer_type === 'existing') {
-        //MAKE AJAX CALL TO SERVER AND GET LIST OF CUSTOMERS
+        //TODO: MAKE AJAX CALL TO SERVER AND GET LIST OF CUSTOMERS
         let url = "{% url 'sales:customer-list' %}";
 
         $.get(url, function (data) {
@@ -93,9 +96,14 @@ function customerChange() {
             "                  <div class='col-sm-4'>\n" +
             "                    <input type='text' name='name' class='form-control' placeholder='Name of Customer...'>\n" +
             "                  </div>\n" +
-            "                  <label for='date' class='col-sm-1 control-label'>Date</label>\n" +
+            "                  <label for='date' class='col-sm-1 control-label'>Category</label>\n" +
             "                  <div class='col-sm-2'>\n" +
-            "                    <input type='date' name='date' class='form-control' placeholder='Date'>\n" +
+            "                    <select id='category' class='form-control input-sm'>\n" +
+            "                       <option value='cat_gen'>General</option> " +
+            "                       <option value='cat_a'>Category A</option> " +
+            "                       <option value='cat_b'>Category B</option> " +
+            "                       <option value='cat_c'>Category C</option> " +
+            "                    </select>\n" +
             "                  </div>\n" +
             "              </div>\n" +
             "              <div class='row form-group'>\n" +
@@ -113,5 +121,30 @@ function customerChange() {
 }
 
 function saveInvoice() {
+    let items_count = parseInt($('#tbody tr').length);
+    let product_list = [];
+    let final_summary = [];
+    for(let i=1; i <= items_count; i++){
+        if($('#item_total_'+i).text() !== ''){
+            product_list.push({
+                'product': $('#item_name_'+i).val(),
+                'quantity': parseFloat($('#item_quantity_'+i).val()),
+                'mrp': parseFloat($('#item_mrp_'+i).val()),
+                'discount': parseFloat($('#item_discount_'+i).val()),
+                'total': parseFloat($('#item_total_'+i).text()),
+            });
+        }
+    }
+    final_summary.push({
+        'sub_total': parseFloat($('#sub_total').text()),
+        'last_balance': parseFloat($('#last_balance').val()),
+        'p_and_f': parseFloat($('#p_and_f').val()),
+        'round_off': parseFloat($('#round_off').val()),
+        'amount_paid': parseFloat($('#amount_paid').val()),
+        'total_amount': parseFloat($('#total_amount').text()),
+        'remaining_balance': parseFloat($('#remaining_balance').text()),
+        'payment_mode': $('#payment_mode').val(),
+    });
 
+    // TODO: Make AJAX call to server...
 }
